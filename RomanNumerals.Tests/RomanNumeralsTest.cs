2d650566220422b260f234d0bdc2d628872b5d97
tomasmu <tomas.kson@gmail.com>
2018-05-21 21:38:11 +0200

    Initial commit

---
+++ RomanNumerals.Tests/RomanNumeralsTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using RomanNumerals;
+
+namespace RomanNumerals.Tests
+{
+    public class RomanNumeralsTest
+    {
+        [Theory]
+        [InlineData("I", 1)]
+        [InlineData("II", 2)]
+        [InlineData("III", 3)]
+        [InlineData("IV", 4)]
+        [InlineData("V", 5)]
+        [InlineData("VI", 6)]
+        [InlineData("LXVII", 67)]
+        [InlineData("CLXXVIII", 178)]
+        [InlineData("DCCLXXXIV", 784)]
+        [InlineData("MMMDCCCLXXXVIII", 3888)]
+        [InlineData("MMMCMXCIX", 3999)]
+        public void Convert_GivenValidRomanNumeral_ConvertsToNumber(string input, int expected)
+        {
+            var result = RomanConverter.Convert(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(2, "II")]
+        [InlineData(3, "III")]
+        [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [InlineData(6, "VI")]
+        [InlineData(67, "LXVII")]
+        [InlineData(178, "CLXXVIII")]
+        [InlineData(784, "DCCLXXXIV")]
+        [InlineData(3888, "MMMDCCCLXXXVIII")]
+        [InlineData(3999, "MMMCMXCIX")]
+        public void Convert_GivenNumberInRange_ConvertsToRomanNumeral(int input, string expected)
+        {
+            var result = RomanConverter.Convert(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("IIII")]
+        [InlineData("IIV")]
+        [InlineData("VV")]
+        [InlineData("VIV")]
+        [InlineData("Horse")]
+        [InlineData("IXL")]
+        [InlineData("LC")]
+        [InlineData("CDM")]
+        public void Convert_GivenInvalidRomanNumeral_ConvertsToZero(string input)
+        {
+            var result = RomanConverter.Convert(input);
+
+            Assert.Equal(0, result);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(4000)]
+        [InlineData(-1)]
+        [InlineData(123456)]
+        public void Convert_GivenNumberOutsideRange_ConvertsToNullString(int input)
+        {
+            var result = RomanConverter.Convert(input);
+
+            Assert.Null(result);
+        }
+    }
+}
---
+++ RomanNumerals/RomanConverter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanNumerals
+{
+    public static class RomanConverter
+    {
+        static RomanConverter()
+        {
+            InitDictionaries();
+        }
+
+        public static string Convert(int number) => _decimalToRoman.GetValueOrDefault(number);
+        public static int Convert(string number) => _romanToDecimal.GetValueOrDefault(number);
+
+        private static Dictionary<int, string> _decimalToRoman = new Dictionary<int, string>();
+        private static Dictionary<string, int> _romanToDecimal = new Dictionary<string, int>();
+
+        private static Dictionary<int, string> _baseNumerals = new Dictionary<int, string>()
+        {
+            [1] = "I",
+            [4] = "IV",
+            [5] = "V",
+            [9] = "IX",
+
+            [10] = "X",
+            [40] = "XL",
+            [50] = "L",
+            [90] = "XC",
+
+            [100] = "C",
+            [400] = "CD",
+            [500] = "D",
+            [900] = "CM",
+
+            [1000] = "M",
+        };
+
+        const int _maxRomanNumber = 3999;
+        private static void InitDictionaries()
+        {
+            var baseDescending = _baseNumerals.OrderByDescending(pair => pair.Key);
+            for (int i = 1; i <= _maxRomanNumber; i++)
+            {
+                var number = i;
+                var roman = String.Empty;
+                foreach (var basePair in baseDescending)
+                {
+                    while (number >= basePair.Key)
+                    {
+                        roman += basePair.Value;
+                        number -= basePair.Key;
+                    }
+                }
+
+                _decimalToRoman.Add(i, roman);
+                _romanToDecimal.Add(roman, i);
+            }
+        }
+    }
+
+
+    public static class ExtensionMethods
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key) =>
+            dict.TryGetValue(key, out TValue result)
+                ? result
+                : default(TValue);
+    }
+}
